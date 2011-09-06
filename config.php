aee218a91c3c6f8cc0e320dc938aca41c2ebfc59
Denis Veselov <progr-m@yandex.ru>
2011-09-06 16:30:09 +0400

    added config-file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+$VKEMAIL = 'mail@exaple.com';		// vk public page admin email/login
+$VKPWD = 'password';				// vk public page admin password
+$VKPPID = '0000001';				// vk public page id
+
+$CITY = 'Vologda'; 					// http://foreca.ru/Russia/$YACITY
+
+$SLEEPTIME = 2;						// in seconds; + rand(1, 4)
+
+?>
